14aab054b804d91c15fb532668a45fb91363f46b
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-10-29 13:21:45 -0700

    Update Awards.js

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function Awards() {
   return(
     <div>
     <Card>
-  <Card.Header style={{fontWeight: 700}}>Awards</Card.Header>
-  <ListGroup variant="flush">
-    <ListGroup.Item>FIRST PLACE (Sportswriting) - CSUN Journalism Day 2021</ListGroup.Item>
-  </ListGroup>
+  <Card.Title style={{fontWeight: 700}}>Awards</Card.Title>
+    <Card.Text>FIRST PLACE (Sportswriting) - CSUN Journalism Day 2021</Card.Text>
 </Card>
 </div>
