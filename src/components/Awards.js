952f61bdaae61c6af65f1780cc4903adc169a70d
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-10-30 14:10:59 -0700

    Update Awards.js

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function Awards() {
   return(
